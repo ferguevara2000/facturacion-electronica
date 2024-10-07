fdd9ad8839c245b1e3cf4f92ea13ca409ff6eb83
ferguevara2000 <gatoguevara@hotmail.com>
2024-10-06 22:06:23 -0500

    add router link to sidebar buttons

--- Frontend/src/components/Sidebar/sidebar.jsx
+++ Frontend/src/components/Sidebar/sidebar.jsx
@@ -1,23 +1,28 @@
 import Button from '@mui/material/Button';
+import { Link } from 'react-router-dom';
 
 function Sidebar() {
   return (
     <div className="sidebar">
       <ul>
         <li>
-          <Button className="w-100">
-            <span className="icon">
-              <i className="bi bi-house"></i></span>
-              Home 
-          </Button>
+          <Link to="/">
+            <Button className="w-100">
+                <span className="icon">
+                <i className="bi bi-house"></i></span>
+                Home 
+            </Button>
+          </Link>
         </li>
         <li>
-          <Button className="w-100">
-            <span className="icon">
-              <i className="bi bi-columns-gap"></i></span>
-              Productos 
-              <span className="arrow"><i className="bi bi-chevron-right"></i></span>
-          </Button>
+          <Link to="/products">
+            <Button className="w-100">
+                <span className="icon">
+                <i className="bi bi-columns-gap"></i></span>
+                Productos 
+                <span className="arrow"><i className="bi bi-chevron-right"></i></span>
+            </Button>
+          </Link>
         </li>
       </ul>
     </div>
