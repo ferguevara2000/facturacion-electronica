7867088cea22652651a1ce2c476838a1bc550ac6
ferguevara2000 <gatoguevara@hotmail.com>
2024-10-11 09:32:23 -0500

    Create submenu in sidebar component

--- Frontend/src/components/Sidebar/sidebar.jsx
+++ Frontend/src/components/Sidebar/sidebar.jsx
@@ -1,10 +1,15 @@
 import Button from '@mui/material/Button';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 function Sidebar() {
 
-  const isOpenSubmenu = () => {
-    alert()
+  const [activeTab, setActiveTab] = useState(0)
+  const [isToggleSubMenu, setIsToggleSubMenu] = useState(false)
+
+  const isOpenSubmenu = (index) => {
+    setActiveTab(index); 
+    setIsToggleSubMenu(!isToggleSubMenu); 
   }
 
   return (
@@ -12,7 +17,7 @@ function Sidebar() {
       <ul>
         <li>
           <Link to="/">
-            <Button className="w-100">
+            <Button className={`w-100 ${activeTab === 0 ? 'active' : ''}`}>
                 <span className="icon">
                 <i className="bi bi-house"></i></span>
                 Home 
@@ -21,7 +26,7 @@ function Sidebar() {
         </li>
         <li>
           <Link to="/products">
-            <Button className="w-100">
+            <Button className={`w-100 ${activeTab === 1 ? 'active' : ''}`}>
                 <span className="icon">
                 <i className="bi bi-columns-gap"></i></span>
                 Productos 
@@ -30,19 +35,29 @@ function Sidebar() {
           </Link>
         </li>
         <li>
-            <Button className="w-100" onClick={isOpenSubmenu}>
+            <Button className={`w-100 ${activeTab === 2 && isToggleSubMenu === true ? 'active' : ''}`} onClick={() => isOpenSubmenu(2)}>
                 <span className="icon">
                 <i className="bi bi-columns-gap"></i></span>
-                Productos 
-                <span className="arrow"><i className="bi bi-chevron-right"></i></span>
+                Menu Desp. 
+                <span className="arrow"><i className={`bi bi-chevron-${isToggleSubMenu === true ? 'down' : 'right'}`}></i></span>
             </Button>
-            <div className="submenuWrapper">
+            <div className={`submenuWrapper ${activeTab === 2 && isToggleSubMenu === true ? 'colapse' : 'colapsed'}`}>
                 <ul className="submenu">
                     <li><Link to='#'>SubMenu 1</Link></li>
                     <li><Link to='#'>SubMenu 2</Link></li>
                     <li><Link to='#'>SubMenu 3</Link></li>
                 </ul>
             </div>
+        </li>
+        <li>
+          <Link to="/products">
+            <Button className={`w-100 ${activeTab === 3 ? 'active' : ''}`}>
+                <span className="icon">
+                <i className="bi bi-columns-gap"></i></span>
+                Productos 
+                <span className="arrow"><i className="bi bi-chevron-right"></i></span>
+            </Button>
+          </Link>
         </li>
       </ul>
     </div>
